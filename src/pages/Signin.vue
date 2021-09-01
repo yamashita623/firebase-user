bffb69866377fd59a10622bebd19020a2a3d75e6
yamashita623 <623msk@gmail.com>
2021-09-01 20:59:10 +0900

    npm run serve時にエラーが出る

---
+++ src/pages/Signin.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="signin">
+    <h2>ログイン画面</h2>
+    <table>
+    <tr>
+      <th>メールアドレス:</th>
+    <input type="email" placeholder="example@example.com" v-model="mailaddress">
+    </tr>
+    <tr>
+      <th>パスワード:</th>
+    <input type="password" placeholder="Password" v-model="password">
+    </tr>
+    </table>
+    <br>
+    <button class="button-login">ログイン</button>
+      <router-link to="/signup">新規登録はこちらから</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Signin',
+  data () {
+    return {
+      mailaddress: '',
+      password: ''
+    }
+  },
+  methods: {}
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+a {
+  color: #4297b9;
+}
+.signin {
+  margin-top: 20px;
+
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center
+}
+button-login {
+  color: #4297b9;
+}
+
+</style>
--- src/router.js
+++ src/router.js
@@ -1,33 +1,14 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Signup from '@/components/Signup'
-import Signin from '@/components/Signin'
-import firebase from 'firebase'
- 
-Vue.use(Router)
- 
-let router = new Router({
-    routes: [
-        {
-            path: '*',
-            redirect: 'signin'
-        },
-        {
-            path: '/signup',
-            name: 'Signup',
-            component: Signup
-        },
-        {
-            path: '/signin',
-            name: 'Signin',
-            component: Signin
-        },
-        {
-            path: '/firebase',
-            name: 'firebase',
-            component: firebase
-        },
-    ]
-})
- 
-export default router
+import Vue from 'vue';
+import Router from 'vue-router';
+import Signin from './pages/Signin.vue';
+import Signup from './pages/Signup.vue';
+
+Vue.use(Router);
+
+export default new Router({
+  mode: "history",
+  routes: [
+    { path: '/', component: Signin },
+    { path: '/signup', component: Signup },
+  ]
+});
